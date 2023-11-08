f78319fb073d438aeaddbf3e21402527531619fa
Roy Tinker <roytinker@protonmail.com>
2023-11-08 12:08:01 -0600

    switch apply_template.sh to use bash for consistency (note macos symlinks sh to bash)

--- apply_template.sh
+++ apply_template.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 template="$(readlink -f "$0" | xargs dirname)/template/gnuplot.js"
 
@@ -6,4 +6,4 @@ mv "$1" "${1}.blk"
 
 grep //{{output.js}} -B 999 "$template" > "$1"
 sed 's/document.currentScript/currentScript/g' "${1}.blk" >> "$1"
-grep //{{output.js}} -A 999 "$template" >> "$1" 
+grep //{{output.js}} -A 999 "$template" >> "$1"
